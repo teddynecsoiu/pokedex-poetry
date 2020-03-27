bff102cb0680778a6be3202fcc7573be0b8588a8
Teddy Necsoiu <teddy.necsoiu@gmail.com>
2020-03-27 21:17:56 +0000

    Setup basic config

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask
 from resources.routes import api_bp
 
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
 app.register_blueprint(api_bp)
