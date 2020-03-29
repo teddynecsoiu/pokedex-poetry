df210379b3d38a5012d1c28fed8cb2b9bf1afa19
Teddy Necsoiu <teddy.necsoiu@gmail.com>
2020-03-29 16:08:58 +0100

    Add description extractor and response handler utility.

--- common/util.py
+++ common/util.py
@@ -1,5 +1,11 @@
+import logging
+from flask import make_response 
+from flask_restful import abort
+
+from config import ClientsConfig
+
 def first(iterable, default=None, condition=lambda x: True):
-    """
+    '''
     Returns the first item in the `iterable` that
     satisfies the `condition`.
 
@@ -33,7 +39,7 @@ def first(iterable, default=None, condition=lambda x: True):
     Traceback (most recent call last):
     ...
     StopIteration
-    """
+    '''
 
     try:
         return next(x for x in iterable if condition(x))
@@ -42,3 +48,60 @@ def first(iterable, default=None, condition=lambda x: True):
             return default
         else:
             raise
+
+def extract_description(body):
+    '''
+    Extract the first english description from a nested dictionary.
+    '''
+    pokemon_en_condition=lambda x: x['language']['name'] == 'en'
+    first_en_description = first(
+        body.get('flavor_text_entries'),
+        condition=pokemon_en_condition)
+    
+    # Some strings contains newline artefacts thoughout it
+    pokemon_description = first_en_description['flavor_text'].replace('\n', ' ')
+    return pokemon_description
+
+def get_limit_header(req):
+    '''
+    Return a dictionary with custom rate limit header values
+    for requests coming from either Funtranslation or Pokeapi.
+
+    Requests URLs are checked agains config client URLs.
+    '''
+    headers = {}
+
+    # Pokeapi does not set any rate limition information on the response
+    # so the value must be kept in sync manually
+    if ClientsConfig.POKEAPI_URL in req.url:
+        headers['X-RateLimit-Pokeapi-Limit'] = '100 per IP address per minute'
+
+    if ClientsConfig.FUNTRANSLATION_URL in req.url:
+        headers['X-RateLimit-Pokeapi-Limit'] = '100 per IP address per minute'
+        headers['X-RateLimit-Funtranslations-Limit'] = req.headers.get('X-RateLimit-Limit')
+        headers['X-RateLimit-Funtranslations-Remaining'] = req.headers.get('X-RateLimit-Remaining') 
+
+    return headers
+
+def check_response(req, api_name):
+    '''
+    Handle and log all non 200 requests.
+    '''
+
+    headers = get_limit_header(req)
+    if req.status_code == 404:
+        resp = make_response('', req.status_code)
+        resp.headers.extend(headers)
+        abort(resp)
+    elif req.status_code == 429:  
+        logging.info("To many requests! {} API limit reached!".format(api_name))
+        resp = make_response('', req.status_code)
+        resp.headers.extend(headers)
+        abort(resp)
+    elif req.status_code != 200:
+        logging.error("Unhandled response from Pokeapi API! status={}".format(req.status_code))
+        resp = make_response('', 500)
+        resp.headers.extend(headers)
+        abort(resp)
+    else:
+        pass
